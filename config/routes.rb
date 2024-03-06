733da9b45878019536d1c82171c388d22555f1f2
moneshpx <monesh.s@pixoatic.com>
2024-03-06 16:31:10 +0530

    fix the 301 issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   # Route for initiating Facebook authentication
-  get '/auth/facebook', to: redirect('https://www.facebook.com/v12.0/dialog/oauth?client_id=954493346035032&redirect_uri=hhttp://localhost:3000/auth/facebook/callback&scope=email')
+  get '/auth/facebook', to: redirect('https://www.facebook.com/v12.0/dialog/oauth?client_id=954493346035032&redirect_uri=http://localhost:3000/auth/facebook/callback&scope=email')
 
   # Route for handling the Facebook authentication callback
   get '/auth/facebook/callback', to: 'sessions#create'
